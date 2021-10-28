29506d88f981dbd763456b2b71724354e25de094
imHireki <imhireki@gmail.com>
2021-10-28 12:18:02 -0300

    feat: improve the functions to populate the Icon

--- backend/utils.py
+++ backend/utils.py
@@ -8,6 +8,8 @@ from random import randrange
 import json
 import requests
 from io import BytesIO
+from os.path import splitext
+from random import choice
 
 from apps.galery.models import Icon
 
@@ -24,13 +26,15 @@ def get_img_file(url, img_name):
     img_io = BytesIO(bytes_img)
     return ImageFile(img_io, name=img_name)
     
-def save_data(title, image):
+def save_data(title, image, border, edit):
     user = get_user_model().objects.get(username='admin')
 
     icon = Icon(
         user=user,
         name=title,
         image=image,
+        has_border=border,
+        has_edit=edit
         )
 
     icon.save()
@@ -40,19 +44,26 @@ def populate_model():
     file = open('data.json', 'r')
     data = json.load(file)
     file.close()
-    
+ 
     for key, value in data.items():
         # Set data
         title = value['title']
         image_url = value['image']
         
-        # TODO: Improve img names
-        img_name = f'{title.replace(" ", "-")}.jpg' # Must has file ext
+        name  =  splitext(image_url)[0].split('/')[4:]
+        img_name = f"{''.join(name)}.jpg" # Must has file ext
         image = get_img_file(image_url, img_name)
        
+        border = choice([True, False])
+        edit = choice([True, False])
         # Save data
         try:
-            save_data(title, image)
+            save_data(
+                title,
+                image,
+                border,
+                edit
+                )
         except Exception as e:
             print(e)
  
