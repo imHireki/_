da2ed929ec24f960ddecaf191437064d42cb2fff
imHireki <imhireki@gmail.com>
2021-11-06 12:47:04 -0300

    back-end/feat: optimize IconView's queryset

--- backend/apps/galery/views.py
+++ backend/apps/galery/views.py
@@ -3,6 +3,7 @@ app galery api views
 """
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .paginators import IconPagination
 from .serializers import IconSerializer
@@ -10,7 +11,7 @@ from .models import Icon
 
 
 class IconView(ListCreateAPIView):
-    queryset = Icon.objects.all()
+    queryset = Icon.objects.all().select_related('user')
     serializer_class = IconSerializer
     pagination_class = IconPagination 
     
