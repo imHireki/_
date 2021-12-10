c23e13a3181d243cb9566224f85d3da80c02cf12
imHireki <imhireki@gmail.com>
2021-12-10 16:22:32 -0300

    front-end/fix: remove bottomOfWindow offset

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -22,60 +22,56 @@
 </template>
 
 <script>
-  import axios from 'axios'
-  import IconBox from '../components/IconBox.vue'
+import axios from 'axios'
+import IconBox from '../components/IconBox.vue'
 
-  export default {
-      name: 'Home',
-      data() {
-        return {
-          icons: [],
-          page: 1,
-          hasNextPage: true,
+export default {
+  name: 'Home',
+  data() {
+    return {
+      icons: [],
+      page: 1,
+      hasNextPage: false
+    }
+  },
+  components: {
+    IconBox
+  },
+  mounted() {
+    this.getIcons()
+    this.getNextPage()
+  },
+  methods: {
+    getIcons() {
+      axios
+        .get(`http://127.0.0.1:8000/api/v1/icons/?page=${this.page}`)
+        .then(response => {
+          for (let i = 0; i < response.data.results.length; i++) {
+            this.icons.push(response.data.results[i])
+          }
+
+          if (response.data.next) {
+            this.hasNextPage = true
+          } else {
+            this.hasNextPage = false
+          }
+        })
+    },
+    getNextPage() {
+      window.onscroll = () => {
+        let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+        if (bottomOfWindow && this.hasNextPage) {
+          this.page += 1
+          this.getIcons()
         }
-      },
-      components: {
-        IconBox
-      },
-      methods: {
-        getIcons() {
-          axios
-            .get(`http://127.0.0.1:8000/api/v1/icons/?page=${this.page}`)
-            .then(response => {
-              for (let i = 0; i < response.data.results.length; i++) {
-                  this.icons.push(response.data.results[i])
-              }
-              if (!response.data.next) {
-                  this.hasNextPage = false
-              }
-            }) 
-            .catch(error => {
-              console.log(error)
-            })
-        },
-        getNextPage() {
-          window.onscroll = () => {
-            let bottomOfWindow = ( document.documentElement.scrollTop + ( window.innerHeight + ( window.innerHeight * 0.2 ) ) ) >= document.documentElement.offsetHeight
-
-            if (bottomOfWindow && this.hasNextPage) {
-              this.page += 1
-              this.getIcons()
-            }
-
-          }        
-        }
-      },
-      beforeMount() {
-        this.getIcons()
-      },
-      mounted() {
-        this.getNextPage()
       }
+    }
   }
+}
 </script>
 
 <style> /* <-- remove scoped here if you have it*/
-   .v-carousel__controls__item{
-      color: #FF1744 !important
-   }
+  .v-carousel__controls__item{
+    color: #FF1744 !important
+  }
 </style>
