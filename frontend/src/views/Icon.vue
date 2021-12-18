bc44865f822a8dae269beefbbdaabd4492e9d75d
imHireki <imhireki@gmail.com>
2021-12-18 17:05:31 -0300

    front-end/feat: add the base for icon detail

---
+++ frontend/src/views/Icon.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="page-icon">
+    {{ icon  }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Icon',
+    data() {
+        return {
+           icon: {}
+        }
+    },
+    mounted() {
+        this.getIcon()
+    },
+    methods: {
+        getIcon() {
+            const slug = this.$route.params.slug
+
+            axios
+                .get(`http://127.0.0.1:8000/api/v1/icons/${slug}`)
+                .then(response => {
+                    this.icon = response.data
+                })
+        }
+    }
+
+}
+</script>
