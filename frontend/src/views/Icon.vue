80c8c33858b0a4e968c569379b5d66458c502af0
imHireki <imhireki@gmail.com>
2021-12-19 19:36:30 -0300

    front-end/fix: add responsive carousel/image

--- frontend/src/views/Icon.vue
+++ frontend/src/views/Icon.vue
@@ -3,37 +3,67 @@
 
     <v-row>
       <v-col>
-        <v-carousel
-          :hide-delimiter-background="true"
-          :show-arrows="false"
-          height="auto"
-        >
-          <v-carousel-item
-            v-for="image in icon.images"
-            :key="image.id"
-          >
-            <v-img
-              :style="{'background-color': image.color}"
-              alt=""
-              :src="image.get_image"
-              :aspect-ratio="1/1"
-              contain
+        <template v-if="check_len_images(icon.images)">
+            <v-carousel
+            :hide-delimiter-background="true"
+            :show-arrows="false"
+            height="auto"
             >
-              <template v-slot:placeholder>
-                <v-row
-                  class="fill-height ma-0"
-                  align="center"
-                  justify="center"
+            <v-carousel-item
+                v-for="image in icon.images"
+                :key="image.id"
+            >
+
+                <v-img
+                class="rounded-circle"
+                :style="{'background-color': image.color}"
+                alt=""
+                :src="image.get_image_256x"
+                :aspect-ratio="1/1"
+                contain
                 >
+                <template v-slot:placeholder>
+                    <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                    >
                     <v-progress-circular
                         indeterminate
                         color="red accent-3"
                     ></v-progress-circular>
+                    </v-row>
+                </template>
+                </v-img>
+
+            </v-carousel-item>
+            </v-carousel>
+        </template>
+
+        <template v-else>
+            <v-img
+            :style="{'background-color': image.color}"
+            alt=""
+            v-for="image in icon.images"
+            :key="image.id"
+            :src="image.get_image_256x"
+            :aspect-ratio="1/1"
+            contain
+            >
+            <template v-slot:placeholder>
+                <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+                >
+                <v-progress-circular
+                    indeterminate
+                    color="red accent-3"
+                ></v-progress-circular>
                 </v-row>
-              </template>
+            </template>
             </v-img>
-          </v-carousel-item>
-        </v-carousel>
+        </template>
       </v-col>
 
       <v-col>
@@ -71,26 +101,32 @@
 import axios from 'axios'
 
 export default {
-    name: 'Icon',
-    data() {
-        return {
-           icon: {}
-        }
-    },
-    mounted() {
-        this.getIcon()
-    },
-    methods: {
-        getIcon() {
-            const slug = this.$route.params.slug
-
-            axios
-                .get(`http://127.0.0.1:8000/api/v1/icons/${slug}`)
-                .then(response => {
-                    this.icon = response.data
-                })
-        }
+  name: 'Icon',
+  data() {
+    return {
+      icon: {}
     }
+  },
+  mounted() {
+    this.getIcon()
+  },
+  methods: {
+    getIcon() {
+      const slug = this.$route.params.slug
 
+        axios
+          .get(`http://127.0.0.1:8000/api/v1/icons/${slug}`)
+          .then(response => {
+             this.icon = response.data
+          })
+        },
+    check_len_images(images) {
+      if(images.length > 1) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
 }
 </script>
