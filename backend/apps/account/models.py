4d58010f7af818fe162070440db962f850fa5549
imHireki <imhireki@gmail.com>
2021-12-21 20:54:03 -0300

    back-end/feat: add create_user at the manager

--- backend/apps/account/models.py
+++ backend/apps/account/models.py
@@ -7,8 +7,10 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-
-    ...
+    def create_user(self, email, password=None):
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
 
 class User(AbstractBaseUser):
     """
