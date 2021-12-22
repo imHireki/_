eae7af010f79837ec14048420a10d0de5693438b
imHireki <imhireki@gmail.com>
2021-12-22 11:28:27 -0300

    back-end/fix: make the manager return user instance

--- backend/apps/account/models.py
+++ backend/apps/account/models.py
@@ -7,13 +7,14 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        user = self.model(email=self.normalize_email(email))
+    def create_user(self, email, password=None, **kwargs):
+        user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
     def create_superuser(self, email, password=None):
-        return self.create_user(email=email, password=password)
+        return self.create_user(email=email, password=password, is_staff=True)
 
 
 class User(AbstractBaseUser):
@@ -22,18 +23,23 @@ class User(AbstractBaseUser):
 
     ABU_FIELDS = `last_login`, `password`
 
+    is_staff needed to see the auth/users/
+
     TODO: create a relationship to different sizes of icon
     TODO: use the email to set the username
+    TODO: add icon
     """
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=30, blank=True)
+
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
 
     class Meta:
-        ordering = ['username']
+        ordering = ['email']
 
     def __str__(self):
-        return self.username
+        return self.email
