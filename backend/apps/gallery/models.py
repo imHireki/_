7088f44c42537981a275b2f476fdfa7490769d9c
imHireki <imhireki@gmail.com>
2022-01-25 10:16:54 -0300

    backend/feat: add feat to resize in a bulk, from image_chan

--- backend/apps/gallery/models.py
+++ backend/apps/gallery/models.py
@@ -7,7 +7,10 @@ from django.utils.text import slugify
 from django.conf import settings
 from django.db import models
 
-from .utils.save import before_save
+import PIL.Image
+from time import time_ns
+from image_chan import image
+from django.core.files import File
 
 user = get_user_model()
 CASCADE = models.CASCADE
@@ -43,7 +46,6 @@ class IconImage(models.Model):
 
     # Original Image. NOTE: Do not modify. Base to all other sizes.
     image = models.ImageField(upload_to='icons/full/%Y/%m/%d')
-
     image_512x = models.ImageField(upload_to='icons/512x/%Y/%m/%d/', blank=True)
     image_256x = models.ImageField(upload_to='icons/256x/%Y/%m/%d/', blank=True)
     image_128x = models.ImageField(upload_to='icons/128x/%Y/%m/%d/', blank=True)
@@ -56,7 +58,7 @@ class IconImage(models.Model):
 
     def __str__(self):
         return 'Image {} from Icon {}'.format(self.pk, self.icon)
- 
+
     def get_image(self):
         return '{}{}'.format(BACKEND_URL, self.image.url)
 
@@ -73,6 +75,30 @@ class IconImage(models.Model):
         return '{}{}'.format(BACKEND_URL, self.image_80x.url)
 
     def save(self, *args, **kwargs):
-        before_save(self)
-        super().save(*args, **kwargs) 
+        # Get resized images' bytes objects
+        with PIL.Image.open(self.image) as img:
+            images = image.Bulk([
+                image.Image.icon(img=img, size=size, quality=75)
+                for size in [(512, 512),
+                             (256, 256),
+                             (128, 128),
+                             (80 , 80 ),
+                             img.size]
+            ]).resize()
 
+        # Convert 'em to django Files
+        rn = time_ns()
+        files = [File(image, '{}.jpg'.format(rn))
+                 for image in images]
+
+        self.image_512x = files[0]
+        self.image_256x = files[1]
+        self.image_128x = files[2]
+        self.image_80x  = files[3]
+        self.image      = files[4]
+
+        # Set color with the smallest image (4 performance improvements)
+        with PIL.Image.open(self.image_80x) as i:
+            self.color = image.GetColors(i).dominant_color()
+
+        super().save(*args, **kwargs)
